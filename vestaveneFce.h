c76c723f53843591a0748dd86c82e23edde74816
davidus6 <davejane@seznam.cz>
2016-12-09 17:05:20 +0100

    Add files via upload

---
+++ vestaveneFce.h
@@ -0,0 +1,18 @@
+
+
+
+int readInt ();
+/*
+double readDouble ();*/
+char *readString ();
+
+int length(char *s);
+int compare(char * s1, char * s2);
+int find(char * s, char * search);
+
+char *substr(char * s, int i, int n);
+
+char *sort(char *);
+void shellSort(char [],int);
+
+void print(int);
