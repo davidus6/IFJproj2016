ed97762eec80739def76650845a411044a2beb7a
davidus6 <davejane@seznam.cz>
2016-12-10 20:04:17 +0100

    Add files via upload

---
+++ functions.h
@@ -0,0 +1,23 @@
+/***** functions.h *****
+*
+*   Implementace interpretu imperativního jazyka IFJ16
+*   Tým 029, varianta b/3/I
+*   Autori: Janecek David, Jiruska Adam
+*   
+*/
+
+
+int ifj16readInt ();
+double ifj16readDouble ();
+char *ifj16readString ();
+
+int ifj16length(char *s);
+int ifj16compare(char * s1, char * s2);
+int ifj16find(char * s, char * search);
+
+char *ifj16substr(char * s, int i, int n);
+
+char *ifj16sort(char *);
+void ifj16shellSort(char [],int);
+
+void ifj16print(char *);
